e0124ac132853d0a61526168adbb567056433fd1
Sindhu <narsingsindhu@gmail.com>
2026-05-28 14:19:33 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3 + 3)  we are using nodejs , not browser 
+
+let name = " Sindhu " //String
+let age = 18 //int 
+let isLoggedIn = false //boolean
+
+//null --> standalone value , representation of empty value Ex:
+let state = null;
+//undefined --> value not assigned 
+//symbol --> unique 
+//object 
+
+console.log(typeof "hi") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
